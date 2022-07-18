c9b0c2732840af08cc0f1fc20612781ee66f507f
LAPTOP-5835AAD2\ferna <fernandojmichel@outlook.com>
2022-07-18 11:08:33 -0500

    Intento de Fetch

--- src/Components/Characters.js
+++ src/Components/Characters.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import useDataFetching from "../Hooks/useDataFetching";
+import task from "../Data/data2.json"; 
 
 function Characters(props) {
+    const [currentPage, setCurrentPage] = useState("https://rickandmortyapi.com/api/character");
+    const [pages, setPages] = useState();
+    const [loading, error, data] = useDataFetching(task);
+    //const [countPage, setCountPage] = useState(1);
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        setCharacters(data);
+    }, [data])
+    console.log(characters);
+
     return (
+        
         <div>
             <h1>CHARACTERS!</h1>
+            <p>{characters}</p>
         </div>
     );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Carousel from './Carousel';
+import Planetas from '../shared/images/Planetas.png';
+import Personajes from '../shared/images/Personajes.png';
+import {Link} from 'react-router-dom';
 import './Home.css';
 
 function Home(props) {
@@ -8,13 +11,46 @@ function Home(props) {
             <div className = "Carousel">
                 <Carousel/>
             </div>
+
             <div className = "Nosotros">
-                <h1 className = "Titulo">¿Quienes Somos?</h1>
+                <h1 className = "Titulo">¿quienes Somos?</h1>
                 <h2 className = "Texto">
                     Somos fan de Star Wars que quieres aportar su parte al fandom hispano con esta enorme
                     fuente de informacion para los fans y para aquellos que quieran entrar en ella. Tu puedes ser uno de nosotros.
                 </h2>
                 <h2 className = "Texto">Ayudamos a que esta API de Star Wars se vuelva la mas completa de todas y que crezca dia a dia.</h2>
+            </div>
+        
+            <div className = "Botones">
+                <Link to="/planetas">
+                    <div className = 'col-5'>
+                        <div className = 'card mb-3'>
+                            <img 
+                                src={Planetas}
+                                alt="Planetas"
+                                className='Planetas'
+                            />
+                            <div className = 'card-body'>
+                                <h2 className = 'card-title'>Planetas</h2>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+
+                <Link to="/personajes">
+                    <div className = 'col-5'>
+                        <div className = 'card mb-3'>
+                            <img 
+                                src={Personajes}
+                                alt="Personajes"
+                                className='Personajes'
+                            />
+                            <div className = 'card-body'>
+                                <h2 className = 'card-title'>Personajes</h2>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
             </div>
         </div>
     );
